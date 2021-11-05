aae2f936ef288277d09a159e1a2cdac0eee31bc9
Audrey Schmidt <audsch@outlook.com>
2021-11-05 14:49:26 -0500

    Made get_current_selections function to find current destination, etc. Appended to new list to use for future. Updated all variables to include list index.

--- main.py
+++ main.py
@@ -18,48 +18,58 @@ def random_choice_single(choice_list):
 def get_new_choice(previous_value, choices):
     needs_new = True
     while needs_new == True:
-            destination = random_choice_single(choices)
-            if(previous_value != destination):
+            new_choice = random_choice_single(choices)
+            if(previous_value != new_choice):
                 needs_new = False
-    return destination
+    return new_choice
 
 #current selections/trip display
-def trip_display(destination, restaurant, transport, entertainment):
+def trip_display(current_selections):
     if confirmed == False:
-        print(f'\nThe following trip has been selected for you:\nDestination: {destination}, Restaurant: {restaurant}, Transport: {transport}, Entertainment: {entertainment}')
+        print(f'\nThe following trip has been selected for you:\nDestination: {my_current_trip[0]}, Restaurant: {my_current_trip[1]}, Transport: {my_current_trip[2]}, Entertainment: {my_current_trip[3]}')
     else: 
-        print (f'The following trip has been completed: You traveled to {destination} in a {transport}. You ate at a {restaurant} and saw a {entertainment}. \n')
+        print('Going on trip...\n')
+        print (f'The following trip has been completed: You traveled to {my_current_trip[0]} in a {my_current_trip[2]}. You ate at a {my_current_trip[1]} and saw a {my_current_trip[3]}. \n')
 
-destination = random_choice_single(destinations)
-restaurant = random_choice_single(restaurants)
-transport = random_choice_single(mode_of_transport)
-entertainment  = random_choice_single(entertainments)
+def get_current_selections(destination_list, restaurant_list, transport_list, entertainment_list):
+    current_selections = []
 
-current_trip = trip_display(destination,restaurant, transport, entertainment)
+    destination = random_choice_single(destination_list)
+    restaurant = random_choice_single(restaurant_list)
+    transport = random_choice_single(transport_list)
+    entertainment = random_choice_single(entertainment_list)
+
+    current_selections.append(destination)
+    current_selections.append(restaurant)
+    current_selections.append(transport)
+    current_selections.append(entertainment)
+
+    return current_selections
+
+my_current_trip = get_current_selections(destinations, restaurants, mode_of_transport, entertainments)
+trip_display(my_current_trip)
 
 #while loop through selections until confirmed by user
 while confirmed == False:
     change = int(input('\nWhat would you like to change about your trip?\n0-Nothing, confirm my trip!, 1-Destination, 2-Restaurant, 3-Transport, 4-Entertainment: '))
     if change == 0:
-        print (f'\nThe following trip has been confirmed for you:\nDestination: {destination}, Restaurant: {restaurant}, Transport: {transport}, Entertainment: {entertainment} \n')
+        print (f'\nThe following trip has been confirmed for you:\nDestination: {my_current_trip[0]}, Restaurant: {my_current_trip[1]}, Transport: {my_current_trip[2]}, Entertainment: {my_current_trip[3]} \n')
         confirmed = True
     elif change == 1:
-        destination = get_new_choice(destination, destinations)
-        trip_display(destination, restaurant, transport, entertainment)
+        my_current_trip[0] = get_new_choice(my_current_trip[0], destinations)
+        trip_display(my_current_trip)
     elif change == 2:
-        restaurant = get_new_choice(restaurant, restaurants)
-        trip_display(destination, restaurant, transport, entertainment)
+        my_current_trip[1] = get_new_choice(my_current_trip[1], restaurants)
+        trip_display(my_current_trip)
     elif change == 3:
-        transport = get_new_choice(transport, mode_of_transport)
-        trip_display(destination, restaurant, transport, entertainment)
+        my_current_trip[2] = get_new_choice(my_current_trip[2], mode_of_transport)
+        trip_display(my_current_trip)
     elif change == 4:
-        entertainment = get_new_choice(entertainment, entertainments)
-        trip_display(destination, restaurant, transport, entertainment)
+        my_current_trip[3] = get_new_choice(my_current_trip[3], entertainments)
+        trip_display(my_current_trip)
 
 #completed trip display using above function
-print('Going on trip...\n')
-
-completed_trip = trip_display(destination, restaurant, transport, entertainment)    
+completed_trip = trip_display(my_current_trip) 
 
 
 
